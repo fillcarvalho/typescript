9bf73058f9984dae873e4640e717604587250942
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 15:24:09 -0300

    Playinf with function type

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -4,4 +4,8 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
-printResult(add(5, 12));
+// let combinedValues: Function;
+var combinedValues;
+combinedValues = add;
+combinedValues = printResult;
+console.log(combinedValues(8, 8));
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -6,4 +6,13 @@ function printResult( num: number ): void {
     console.log( 'Result: ' + num );
 }
 
-printResult(add(5,12))
+
+
+// let combinedValues: Function;
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+// combinedValues = printResult;
+
+
+console.log(combinedValues(8,8));
