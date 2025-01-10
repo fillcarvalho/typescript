1f3cb8b31ebd54f21722185f314b3c500bb25b9e
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 15:06:41 -0300

    Working with type aliases and CUstom Types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,16 +1,17 @@
 function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedNumbers = combine(30, 26, 'as-number');
+var combinedNumbers = combine(30, 26, "as-number");
 console.log(combinedNumbers);
-var combinedStringNumbers = combine('30', '26', 'as-number');
+var combinedStringNumbers = combine("30", "26", "as-number");
 console.log(combinedStringNumbers);
-var combinedNames = combine('Max', 'Anna', 'as-text');
+var combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,20 +1,33 @@
-function combine(input1: string | number, input2: string | number, resultConversion: 'as-number' | 'as-text') {
-    let result;
+type Combinable = number | string;
+type ConversionDescriptor =  'as-number' | 'as-text';
 
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    
-    return result;
+// type User = { name: string; age: number };
+// const u1: User = { name: 'Max', age: 30 }; // this works!
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
 }
 
-const combinedNumbers = combine(30,26, 'as-number');
+const combinedNumbers = combine(30, 26, "as-number");
 console.log(combinedNumbers);
 
-const combinedStringNumbers = combine('30','26', 'as-number');
+const combinedStringNumbers = combine("30", "26", "as-number");
 console.log(combinedStringNumbers);
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
