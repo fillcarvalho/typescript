3c0d9d9b3405b356430131daf796ffd18d0517dd
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 15:54:43 -0300

    Working with callbackfunction and function patter definition

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -1,12 +1,15 @@
-function add(n1: number, n2: number){
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-function printResult( num: number ): void {
-    console.log( 'Result: ' + num );
+function printResult(num: number): void {
+  console.log("Result: " + num);
 }
 
-
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
 // let combinedValues: Function;
 let combinedValues: (a: number, b: number) => number;
@@ -14,5 +17,5 @@ let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
 // combinedValues = printResult;
 
-
-console.log(combinedValues(8,8));
+// console.log(combinedValues(8, 8));
+addAndHandle(10, 8, (result) => console.log(result));
