34b96892c02433197cd5ecaf8786e7b93a59c234
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 15:13:37 -0300

    Paying with void and undefined return types

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,17 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
-        resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedNumbers = combine(30, 26, "as-number");
-console.log(combinedNumbers);
-var combinedStringNumbers = combine("30", "26", "as-number");
-console.log(combinedStringNumbers);
-var combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
