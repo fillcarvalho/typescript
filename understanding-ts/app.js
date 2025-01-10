90540022003dd41835ca11af9e312df34ed22a43
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 16:36:15 -0300

    Configuting the tsconfig.json, changing exlude parameter and ignoring old ts files we won't use anymore

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,16 +1,2 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log("Result: " + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-// let combinedValues: Function;
-var combinedValues;
-combinedValues = add;
-// combinedValues = printResult;
-// console.log(combinedValues(8, 8));
-addAndHandle(10, 8, function (result) { return console.log(result); });
+"use strict";
+console.log("app11aaa1js");
