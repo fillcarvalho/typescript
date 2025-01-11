7a02ed12f64f55c13aa122c7ae5c88db1662c1fd
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 22:14:46 -0300

    Working with inheritance

--- section-5/dist/app.js
+++ section-5/dist/app.js
@@ -9,7 +9,7 @@ class Department {
      * Describes the object
      */
     describe() {
-        console.log('Department: ' + this.name);
+        console.log("Department: " + this.name);
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -19,11 +19,11 @@ class Department {
         console.log(this.employees);
     }
 }
-const dep1 = new Department('Department 1');
+const dep1 = new Department("Department 1");
 console.log(dep1);
 dep1.describe();
-dep1.addEmployee('Filipe');
-dep1.addEmployee('Lucas');
+dep1.addEmployee("Filipe");
+dep1.addEmployee("Lucas");
 // This should be avoided because can bring insecurities to your code. What is the addEmployee also does something
 // else inside of the function?
 // By adding private in the front of the method, you avoid things like that
@@ -32,18 +32,46 @@ dep1.addEmployee('Lucas');
 dep1.printEmployeeInformation();
 // const describeMethod = {name: 'Dummy department', describe: dep1.describe};
 // describeMethod.describe();
-// As we added the this on the describe function at the class, passing it without the name property it will 
+// As we added the this on the describe function at the class, passing it without the name property it will
 // throw an error
 // const describeMethod2 = {describe: dep1.describe};
 // describeMethod2.describe();
+// You can declare the class properties directly at the constructor
 class Department2 {
+    // Readyonly add an extra typesafe
     constructor(id, name) {
         this.id = id;
         this.name = name;
     }
     describe() {
         console.log(`Department ${this.id}: ${this.name}`);
+        console.log("Department", this);
     }
 }
 const dep2 = new Department2(1, "Department 2");
 dep2.describe();
+// Working with inheritance
+class ITDepartment extends Department2 {
+    constructor(id, admins) {
+        // If you are planing to use this, you need to call the super function before
+        super(id, "IT Department");
+        this.admins = admins;
+    }
+}
+const itDepartment = new ITDepartment(2, ["Filipe", "Lucas", "Mike"]);
+itDepartment.describe();
+class accountingDepartment extends Department2 {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const accountingDep = new accountingDepartment(4, []);
+accountingDep.addReport('Problem found');
+accountingDep.printReports();
--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -3,16 +3,16 @@ console.log("Section 5");
 class Department {
   private name: string;
   private employees: string[] = [];
-  
+
   constructor(n: string) {
-    this.name = n
+    this.name = n;
   }
 
   /**
    * Describes the object
    */
   describe(this: Department) {
-    console.log('Department: ' + this.name)
+    console.log("Department: " + this.name);
   }
 
   addEmployee(employee: string) {
@@ -20,17 +20,16 @@ class Department {
   }
 
   printEmployeeInformation() {
-    console.log(this.employees.length)
-    console.log(this.employees)
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
-
 }
 
-const dep1 = new Department('Department 1');
-console.log(dep1)
-dep1.describe()
-dep1.addEmployee('Filipe');
-dep1.addEmployee('Lucas');
+const dep1 = new Department("Department 1");
+console.log(dep1);
+dep1.describe();
+dep1.addEmployee("Filipe");
+dep1.addEmployee("Lucas");
 
 // This should be avoided because can bring insecurities to your code. What is the addEmployee also does something
 // else inside of the function?
@@ -42,20 +41,56 @@ dep1.printEmployeeInformation();
 // const describeMethod = {name: 'Dummy department', describe: dep1.describe};
 // describeMethod.describe();
 
-// As we added the this on the describe function at the class, passing it without the name property it will 
+// As we added the this on the describe function at the class, passing it without the name property it will
 // throw an error
 // const describeMethod2 = {describe: dep1.describe};
 // describeMethod2.describe();
 
 // You can declare the class properties directly at the constructor
 class Department2 {
-    constructor( private id: number, private name: string ) {}
+  // Readyonly add an extra typesafe
+  constructor(private readonly id: number, private name: string) {}
 
-    describe( this: Department2 ) {
-        console.log( `Department ${this.id}: ${this.name}` )
-    }
+  describe(this: Department2) {
+    console.log(`Department ${this.id}: ${this.name}`);
+    console.log("Department", this);
+  }
 }
 
 const dep2 = new Department2(1, "Department 2");
 
 dep2.describe();
+
+// Working with inheritance
+
+class ITDepartment extends Department2 {
+  constructor(id: number, public admins: string[]) {
+    // If you are planing to use this, you need to call the super function before
+    super(id, "IT Department");
+  }
+}
+
+const itDepartment = new ITDepartment(2, ["Filipe", "Lucas", "Mike"]);
+
+itDepartment.describe();
+
+class accountingDepartment extends Department2 {
+  constructor(id: number, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+
+const accountingDep = new accountingDepartment(4, []);
+
+accountingDep.addReport('Problem found');
+
+accountingDep.printReports();
