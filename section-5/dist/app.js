29664bea2fb8a3205424c5f4b875f12842868d08
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 21:47:09 -0300

    Working with private properties

--- section-5/dist/app.js
+++ section-5/dist/app.js
@@ -2,6 +2,7 @@
 console.log("Section 5");
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     /**
@@ -10,12 +11,26 @@ class Department {
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const dep1 = new Department('Department 1');
 console.log(dep1);
 dep1.describe();
-const describeMethod = { name: 'Dummy department', describe: dep1.describe };
-describeMethod.describe();
+dep1.addEmployee('Filipe');
+dep1.addEmployee('Lucas');
+// This should be avoided because can bring insecurities to your code. What is the addEmployee also does something
+// else inside of the function?
+// By adding private in the front of the method, you avoid things like that
+// dep1.employees.push('Mike');
+dep1.printEmployeeInformation();
+// const describeMethod = {name: 'Dummy department', describe: dep1.describe};
+// describeMethod.describe();
 // As we added the this on the describe function at the class, passing it without the name property it will 
 // throw an error
 // const describeMethod2 = {describe: dep1.describe};
--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -2,6 +2,7 @@ console.log("Section 5");
 
 class Department {
   name: string;
+  private employees: string[] = [];
   
   constructor(n: string) {
     this.name = n
@@ -13,14 +14,33 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
+
 }
 
 const dep1 = new Department('Department 1');
 console.log(dep1)
 dep1.describe()
+dep1.addEmployee('Filipe');
+dep1.addEmployee('Lucas');
 
-const describeMethod = {name: 'Dummy department', describe: dep1.describe};
-describeMethod.describe();
+// This should be avoided because can bring insecurities to your code. What is the addEmployee also does something
+// else inside of the function?
+// By adding private in the front of the method, you avoid things like that
+// WIth the private, you can only access this variable inside of the class
+// dep1.employees.push('Mike');
+
+dep1.printEmployeeInformation();
+// const describeMethod = {name: 'Dummy department', describe: dep1.describe};
+// describeMethod.describe();
 
 // As we added the this on the describe function at the class, passing it without the name property it will 
 // throw an error
