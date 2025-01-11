1422c2e08257dca85d7f2ad742eb833513cb8207
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 21:32:46 -0300

    Working with methods

--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -6,8 +6,24 @@ class Department {
   constructor(n: string) {
     this.name = n
   }
+
+  /**
+   * Describes the object
+   */
+  describe(this: Department) {
+    console.log('Department: ' + this.name)
+  }
 }
 
 const dep1 = new Department('Department 1');
-
 console.log(dep1)
+dep1.describe()
+
+const describeMethod = {name: 'Dummy department', describe: dep1.describe};
+describeMethod.describe();
+
+// As we added the this on the describe function at the class, passing it without the name property it will 
+// throw an error
+// const describeMethod2 = {describe: dep1.describe};
+// describeMethod2.describe();
+
