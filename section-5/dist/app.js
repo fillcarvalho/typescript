d160dfd1699d92a309dd6580c83bf7b818d2f0b0
Filipe Costa <filipe@rhizatech.com.br>
2025-01-10 21:18:10 -0300

    Creating my first class

--- section-5/dist/app.js
+++ section-5/dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("Section 5");
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const dep1 = new Department('Department 1');
+console.log(dep1);
--- section-5/src/app.ts
+++ section-5/src/app.ts
@@ -1,1 +1,13 @@
 console.log("Section 5");
+
+class Department {
+  name: string;
+  
+  constructor(n: string) {
+    this.name = n
+  }
+}
+
+const dep1 = new Department('Department 1');
+
+console.log(dep1)
